977173d610c7251ef7fe4bb23a2dd0c99d7a5fa3
Jyoti Dubey <jyoti0801@gmail.com>
2019-07-31 22:46:56 +0530

    Add questions fragment

--- app/src/main/java/com/mindorks/framework/jetpack/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mindorks/framework/jetpack/ui/home/HomeActivity.kt
@@ -1,48 +1,49 @@
 package com.mindorks.framework.jetpack.ui.home
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 import com.mindorks.framework.jetpack.R
 import com.mindorks.framework.jetpack.databinding.ActivityHomeBinding
+
+
 
 /**
  * Created by jyotidubey on 2019-02-27.
  */
-class HomeActivity: AppCompatActivity(){
-
+class HomeActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
-    private lateinit var binding : ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityHomeBinding =  DataBindingUtil.setContentView(this,
             R.layout.activity_home)
+        setSupportActionBar(binding.toolbar)
+
         drawerLayout = binding.drawerLayout
 
-        navController = Navigation.findNavController(this, R.id.drawer_nav_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
 
-        // Set up ActionBar
-        setSupportActionBar(binding.toolbar)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
 
-        // Set up navigation menu
-        binding.navigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(navigationView, navController)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
     }
 
+
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment),drawerLayout);
     }
 
     override fun onBackPressed() {
@@ -52,4 +53,18 @@ class HomeActivity: AppCompatActivity(){
             super.onBackPressed()
         }
     }
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        menuItem.isChecked = true
+        drawerLayout.closeDrawers()
+
+        val id = menuItem.itemId
+        when (id) {
+
+            R.id.logout -> {
+                navController.navigate(R.id.loginFragment)
+            }
+        }
+        return true
+    }
+
 }
